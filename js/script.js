5b7d5678a9a67481e71925e6d49410fd214e7679
vlados552 <vlados552@gmail.com>
2022-09-07 20:06:15 -0700

    Add constants and variables to script file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 /*----- constants -----*/
+const DIFFICULT = 5;
+const GAME_STATE_WELCOME = 0;
+const GAME_STATE_HOWTOPLAY = 1;
+// const GAME_STATE_DIFFICULT = 2;
+const GAME_STATE_GAMEPLAY = 3;
+const GAME_STATE_GAMEOVER = 4;
 /*----- app's state (variables) -----*/
+let gameState;
 /*----- cached element references -----*/
+const btnPlay = document.querySelector('#js-modal-btn-play');
+const btnHowToPlay = document.querySelector('#js-modal-btn-how-to');
+const btnPlayAgain = document.querySelector('#js-modal-game-over-play-again');
+// const btnPickDifficult = document.querySelector('#js-modal-game-over-pick-difficult');
 /*----- event listeners -----*/
 /*----- functions -----*/
+function init() {
+	//
+}
+function render() {
+	//
+}
